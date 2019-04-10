82f8e05b290bf9393a51b676324e1cacbfc24194
William Lennartsson <williamlennartsson@Williams-MacBook-Air.local>
2019-04-10 09:16:57 +0200

    Initial Commit

---
+++ swift-demo-animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-demo-animations
+//
+//  Created by William Lennartsson on 2019-04-10.
+//  Copyright © 2019 William Lennartsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swift-demo-animationsTests/swift_demo_animationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_demo_animationsTests.swift
+//  swift-demo-animationsTests
+//
+//  Created by William Lennartsson on 2019-04-10.
+//  Copyright © 2019 William Lennartsson. All rights reserved.
+//
+
+import XCTest
+@testable import swift_demo_animations
+
+class swift_demo_animationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
